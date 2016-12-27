229169fa056d50b10eadaf1e818523cfecf7d62a
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-12-27 23:16:04 +0900

    printk: use vprintk_func in vprintk()
    vprintk(), just like printk(), better be using per-cpu printk_func
    instead of direct vprintk_emit() call. Just in case if vprintk()
    will ever be called from NMI, or from any other context that can
    deadlock in printk().
    
    Link: http://lkml.kernel.org/r/20161227141611.940-2-sergey.senozhatsky@gmail.com
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Calvin Owens <calvinowens@fb.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Peter Hurley <peter@hurleysoftware.com>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Reviewed-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1922,7 +1922,7 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
-	return vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
+	return vprintk_func(fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
 
