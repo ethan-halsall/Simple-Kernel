10f32f98cb58f9992c14005b71b3640d0f7766ce
Chris Redpath <chris.redpath@arm.com>
2017-10-21 15:56:50 +0100

    ANDROID: sched: Unconditionally honor sync flag for energy-aware wakeups
    Since we don't do energy-aware wakeups when we are overutilized, always
    honoring sync wakeups in this state does not prevent wake-wide mechanics
    overruling the flag as normal.
    
    This patch is based upon previous work to build EAS for android products.
    
    sync-hint code taken from commit 4a5e890ec60d
    "sched/fair: add tunable to force selection at cpu granularity" written
    by Juri Lelli <juri.lelli@arm.com>
    
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    (cherry-picked from commit f1ec666a62dec1083ed52fe1ddef093b84373aaf)
    [ Moved the feature to find_energy_efficient_cpu() ]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Change-Id: I4b3d79141fc8e53dc51cd63ac11096c2e3cb10f5
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -87,14 +87,17 @@ unsigned int sysctl_sched_latency			= 6000000ULL;
 unsigned int normalized_sysctl_sched_latency		= 6000000ULL;
 
 unsigned int sysctl_sched_is_big_little = 1;
-unsigned int sysctl_sched_sync_hint_enable = 1;
-unsigned int sysctl_sched_cstate_aware = 1;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_SCHED_WALT
 unsigned int sysctl_sched_use_walt_cpu_util = 1;
 unsigned int sysctl_sched_use_walt_task_util = 1;
 #endif
+/*
+ * Enable/disable honoring sync flag in energy-aware wakeups.
+ */
+unsigned int sysctl_sched_sync_hint_enable = 1;
+
 /*
  * Enable/disable using cstate knowledge in idle sibling selection
  */
@@ -8209,7 +8212,7 @@ static DEFINE_PER_CPU(cpumask_t, energy_cpus);
  * let's keep things simple by re-using the existing slow path.
  */
 
-static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu)
+static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sync)
 {
 	unsigned long prev_energy = ULONG_MAX, best_energy = ULONG_MAX;
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
@@ -8218,6 +8221,12 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu)
 	struct perf_domain *pd;
 	struct sched_domain *sd;
 	cpumask_t *candidates;
+
+	if (sysctl_sched_sync_hint_enable && sync) {
+		cpu = smp_processor_id();
+		if (cpumask_test_cpu(cpu, &p->cpus_allowed))
+			return cpu;
+	}
 
 	rcu_read_lock();
 	pd = rcu_dereference(rd->pd);
@@ -8312,8 +8321,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 		record_wakee(p);
 
-		new_cpu = find_energy_efficient_cpu(p, prev_cpu);
-			if (new_cpu >= 0)
+		new_cpu = find_energy_efficient_cpu(p, prev_cpu, sync);
+		if (new_cpu >= 0)
 				return new_cpu;
 		new_cpu = prev_cpu;
 
