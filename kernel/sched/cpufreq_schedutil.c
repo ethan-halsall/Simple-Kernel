d0a24c7971792d3c2d5a2f1a450ce29921c6ea07
Leo Yan <leo.yan@linaro.org>
2018-02-08 21:48:22 +0800

    sched/cpufreq: Remove unused SUGOV_KTHREAD_PRIORITY macro
    Since schedutil kernel thread directly set priority to 0, the macro
    SUGOV_KTHREAD_PRIORITY is not used.  So remove it.
    
    Change-Id: Id30d3f40cc25436b26c2c3d6dde069e22fb7da0b
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vikram Mulukutla <markivx@codeaurora.org>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/1518097702-9665-1-git-send-email-leo.yan@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -25,7 +25,6 @@
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
 #define LATENCY_MULTIPLIER			(1000)
-#define SUGOV_KTHREAD_PRIORITY	50
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
