0aa0426c7bd9af9175ff545c54f257381eb526fd
Josh Poimboeuf <jpoimboe@redhat.com>
2018-02-20 11:37:51 -0600

    jump_label: Explicitly disable jump labels in __init code
    After initmem has been freed, any jump labels in __init code are
    prevented from being written to by the kernel_text_address() check in
    __jump_label_update().  However, this check is quite broad.  If
    kernel_text_address() were to return false for any other reason, the
    jump label write would fail silently with no warning.
    
    For jump labels in module init code, entry->code is set to zero to
    indicate that the entry is disabled.  Do the same thing for core kernel
    init code.  This makes the behavior more consistent, and will also make
    it more straightforward to detect non-init jump label write failures in
    the next patch.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Jason Baron <jbaron@akamai.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/c52825c73f3a174e8398b6898284ec20d4deb126.1519051220.git.jpoimboe@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I47a2f5aeef8d9b61710fe61338f49f740f67cf81
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- init/main.c
+++ init/main.c
@@ -82,6 +82,7 @@
 #include <linux/io.h>
 #include <linux/kaiser.h>
 #include <linux/cache.h>
+#include <linux/jump_label.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -960,6 +961,7 @@ static int __ref kernel_init(void *unused)
 	kernel_init_freeable();
 	/* need to finish all async __init code before freeing the memory */
 	async_synchronize_full();
+	jump_label_invalidate_init();
 	free_initmem();
 	mark_readonly();
 	system_state = SYSTEM_RUNNING;
