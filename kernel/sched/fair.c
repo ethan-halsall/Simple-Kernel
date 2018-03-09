6e73ca22f580583d7290b7394647d71a1aad88bc
Patrick Bellasi <patrick.bellasi@arm.com>
2018-03-09 09:52:43 +0000

    BACKPORT: sched/fair: Use util_est in LB and WU paths
    When the scheduler looks at the CPU utilization, the current PELT value
    for a CPU is returned straight away. In certain scenarios this can have
    undesired side effects on task placement.
    
    For example, since the task utilization is decayed at wakeup time, when
    a long sleeping big task is enqueued it does not add immediately a
    significant contribution to the target CPU.
    As a result we generate a race condition where other tasks can be placed
    on the same CPU while it is still considered relatively empty.
    
    In order to reduce this kind of race conditions, this patch introduces the
    required support to integrate the usage of the CPU's estimated utilization
    in the wakeup path, via cpu_util_wake(), as well as in the load-balance
    path, via cpu_util() which is used by update_sg_lb_stats().
    
    The estimated utilization of a CPU is defined to be the maximum between
    its PELT's utilization and the sum of the estimated utilization (at
    previous dequeue time) of all the tasks currently RUNNABLE on that CPU.
    This allows to properly represent the spare capacity of a CPU which, for
    example, has just got a big task running since a long sleep period.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Steve Muckle <smuckle@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Todd Kjos <tkjos@android.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Link: http://lkml.kernel.org/r/20180309095245.11071-3-patrick.bellasi@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    [
     backport from upstream:
     commit f9be3e5 ("sched/fair: Use util_est in LB and WU paths")
    
     This provides also schedutil integration, since:
        sugov_get_util()
           boosted_cpu_util()
              cpu_util_freq()
                 cpu_util()
    
     thus, not requiring to backport:
     commit a07630b ("sched/cpufreq/schedutil: Use util_est for OPP selection")
    
     Support for energy_diff is also provided, since:
        calc_sg_energy()
           find_new_capacity()
              group_max_util()
                 cpu_util_wake()
     and:
           group_norm_util()
              cpu_util_wake()
    
     Where both cpu_util() and cpu_util_wake() already consider the estimated
     utlilization in case of PELT being in use.
    ]
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Change-Id: I2be201cf7bb0b1449b14b4da64844067dbdb5eb4
    Git-commit: f9fb6fb
    Git-repo: https://android.googlesource.com/kernel/common/
    [satyap@codeaurora.org:
    1. Updated cpu_util() as needed and keep the function in
       kernel/sched/sched.h
    2. Trivial merge conflicts resolution for task_util_est() call sites
    ]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    [lazerl0rd: adjusted for kernel 4.9]
    Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3588,6 +3588,11 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 
 static inline unsigned long task_util_est(struct task_struct *p)
 {
+#ifdef CONFIG_SCHED_WALT
+	if (likely(!walt_disabled && sysctl_sched_use_walt_task_util))
+		return (p->ravg.demand /
+			(sched_ravg_window >> SCHED_CAPACITY_SHIFT));
+#endif
 	return max(task_util(p), _task_util_est(p));
 }
 
@@ -5848,6 +5853,35 @@ static unsigned long cpu_util_wake(int cpu, struct task_struct *p)
 
 	/* Discount task's blocked util from CPU's util */
 	util -= min_t(unsigned int, util, task_util(p));
+
+	/*
+	 * Covered cases:
+	 *
+	 * a) if *p is the only task sleeping on this CPU, then:
+	 *      cpu_util (== task_util) > util_est (== 0)
+	 *    and thus we return:
+	 *      cpu_util_wake = (cpu_util - task_util) = 0
+	 *
+	 * b) if other tasks are SLEEPING on this CPU, which is now exiting
+	 *    IDLE, then:
+	 *      cpu_util >= task_util
+	 *      cpu_util > util_est (== 0)
+	 *    and thus we discount *p's blocked utilization to return:
+	 *      cpu_util_wake = (cpu_util - task_util) >= 0
+	 *
+	 * c) if other tasks are RUNNABLE on that CPU and
+	 *      util_est > cpu_util
+	 *    then we use util_est since it returns a more restrictive
+	 *    estimation of the spare capacity on that CPU, by just
+	 *    considering the expected utilization of tasks already
+	 *    runnable on that CPU.
+	 *
+	 * Cases a) and b) are covered by the above code, while case c) is
+	 * covered by the following code when estimated utilization is
+	 * enabled.
+	 */
+	if (sched_feat(UTIL_EST))
+		util = max(util, READ_ONCE(cfs_rq->avg.util_est.enqueued));
 #endif
 
 	/*
@@ -6472,7 +6506,7 @@ schedtune_task_margin(struct task_struct *p)
 	if (boost == 0)
 		return 0;
 
-	util = task_util(p);
+	util = task_util_est(p);
 	margin = schedtune_margin(util, boost);
 
 	return margin;
@@ -6508,7 +6542,7 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 static inline unsigned long
 boosted_task_util(struct task_struct *p)
 {
-	unsigned long util = task_util(p);
+	unsigned long util = task_util_est(p);
 	long margin = schedtune_task_margin(p);
 
 	trace_sched_boost_task(p, util, margin);
@@ -7202,7 +7236,7 @@ retry:
 			 * accounting. However, the blocked utilization may be zero.
 			 */
 			wake_util = cpu_util_wake(i, p);
-			new_util = wake_util + task_util(p);
+			new_util = wake_util + task_util_est(p);
 			spare_cap = capacity_orig_of(i) - wake_util;
 
 			if (spare_cap > most_spare_cap) {
@@ -7722,7 +7756,7 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 		int delta = 0;
 		struct energy_env eenv = {
 			.p              = p,
-			.util_delta     = task_util(p),
+			.util_delta     = task_util_est(p),
 			/* Task's previous CPU candidate */
 			.cpu[EAS_CPU_PRV] = {
 				.cpu_id = prev_cpu,
@@ -7741,7 +7775,7 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 #ifdef CONFIG_SCHED_WALT
 		if (!walt_disabled && sysctl_sched_use_walt_cpu_util &&
 			p->state == TASK_WAKING)
-			delta = task_util(p);
+			delta = task_util_est(p);
 #endif
 		/* Not enough spare capacity on previous cpu */
 		if (__cpu_overutilized(prev_cpu, delta)) {
