48581979ddc6044a1d35b765696e629ed0b36032
Josh Poimboeuf <jpoimboe@redhat.com>
2018-03-19 13:18:57 -0500

    jump_label: Disable jump labels in __exit code
    With the following commit:
    
      333522447063 ("jump_label: Explicitly disable jump labels in __init code")
    
    ... we explicitly disabled jump labels in __init code, so they could be
    detected and not warned about in the following commit:
    
      dc1dd184c2f0 ("jump_label: Warn on failed jump_label patching attempt")
    
    In-kernel __exit code has the same issue.  It's never used, so it's
    freed along with the rest of initmem.  But jump label entries in __exit
    code aren't explicitly disabled, so we get the following warning when
    enabling pr_debug() in __exit code:
    
      can't patch jump_label at dmi_sysfs_exit+0x0/0x2d
      WARNING: CPU: 0 PID: 22572 at kernel/jump_label.c:376 __jump_label_update+0x9d/0xb0
    
    Fix the warning by disabling all jump labels in initmem (which includes
    both __init and __exit code).
    
    Reported-and-tested-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Jason Baron <jbaron@akamai.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: dc1dd184c2f0 ("jump_label: Warn on failed jump_label patching attempt")
    Link: http://lkml.kernel.org/r/7121e6e595374f06616c505b6e690e275c0054d1.1521483452.git.jpoimboe@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I59cfd076b7b5cd62087b1390eb7ab36a1db3f70b
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- init/main.c
+++ init/main.c
@@ -961,7 +961,7 @@ static int __ref kernel_init(void *unused)
 	kernel_init_freeable();
 	/* need to finish all async __init code before freeing the memory */
 	async_synchronize_full();
-	jump_label_invalidate_init();
+	jump_label_invalidate_initmem();
 	free_initmem();
 	mark_readonly();
 	system_state = SYSTEM_RUNNING;
