5d4207e596ca6a5afca166aff36cf0c6acbc63d2
Ionela Voinescu <ionela.voinescu@arm.com>
2018-04-27 18:38:00 +0100

    ANDROID: sched/fair: add idle state filter to prefer_idle case
    The CPU selection process for a prefer_idle task either minimizes or
    maximizes the CPU capacity for idle CPUs depending on the task being
    boosted or not.
    
    Given that we are iterating through all CPUs, additionally filter the
    choice by preferring a CPU in a more shallow idle state. This will
    provide both a faster wake-up for the task and higher energy efficiency,
    by allowing CPUs in deeper idle states to remain idle.
    
    Change-Id: Ic55f727a0c551adc0af8e6ee03de6a41337a571b
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7306,6 +7306,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util, new_util_cuml;
 			long spare_cap;
+			int idle_idx = INT_MAX;
 
 			trace_sched_cpu_util(i);
 			if (!cpu_online(i) || cpu_isolated(i))
@@ -7359,6 +7360,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 */
 			spare_cap = capacity_orig - new_util;
 
+			if (idle_cpu(i))
+				idle_idx = idle_get_state_idx(cpu_rq(i));
+
+
 			/*
 			 * Case A) Latency sensitive tasks
 			 *
@@ -7402,13 +7407,18 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					schedstat_inc(this_rq()->eas_stats.fbt_pref_idle);
 
 					if (boosted &&
-					    capacity_orig <= target_capacity)
+					    capacity_orig < target_capacity)
 						continue;
 					if (!boosted &&
-					    capacity_orig >= target_capacity)
+					    capacity_orig > target_capacity)
+						continue;
+					if (capacity_orig == target_capacity &&
+					    sysctl_sched_cstate_aware &&
+					    best_idle_cstate <= idle_idx)
 						continue;
 
 					target_capacity = capacity_orig;
+					best_idle_cstate = idle_idx;
 					best_idle_cpu = i;
 					continue;
 				}
@@ -7487,8 +7497,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * consumptions without affecting performance.
 			 */
 			if (idle_cpu(i)) {
-				int idle_idx = idle_get_state_idx(cpu_rq(i));
-
 				/*
 				 * Skip CPUs in deeper idle state, but only
 				 * if they are also less energy efficient.
