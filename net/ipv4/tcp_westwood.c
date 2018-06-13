b16703d3273ea48849e16322006472e3e0467197
Harsh Shandilya <harsh@prjkt.io>
2018-06-13 22:16:06 +0300

    tcp_westwood : Bump minimum RTT and lower initial RTT duration
    Slightly bumping the minimum round-trip-time adds more tolerance
    for connections with fluctutating ping to stay connected before
    the sender-side TCP stack gives up on the packet and considers
    it lost and the connection broken.
    
    Lowering the initial RTT duration works conversely by reducing
    tolerance for bad networks so that we avoid getting on bad
    connections in the first place.
    
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,8 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/20)	/* 50ms */
-#define TCP_WESTWOOD_INIT_RTT  (20*HZ)	/* maybe too conservative?! */
+#define TCP_WESTWOOD_RTT_MIN   (HZ/15)	/* 66.6ms */
+#define TCP_WESTWOOD_INIT_RTT  (15*HZ)	/* maybe too conservative?! */
 
 /*
  * @tcp_westwood_create
