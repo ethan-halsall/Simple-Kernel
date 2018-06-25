d856ac8b7a0d66972dcd3f015b9e6adb798768f8
Bruno Martins <bgcngm@gmail.com>
2018-06-25 11:55:49 +0200

    drivers: thermal: Import Xiaomi changes
    Change-Id: I0282f3e147f610c2df540a93d7ae6cb3aa2ca49c
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- drivers/thermal/qpnp-adc-tm.c
+++ drivers/thermal/qpnp-adc-tm.c
@@ -264,8 +264,10 @@ struct qpnp_adc_tm_sensor {
 	bool				thermal_node;
 	uint32_t			scale_type;
 	struct list_head		thr_list;
-	bool				high_thr_triggered;
-	bool				low_thr_triggered;
+	int				high_thr_triggered;
+	int				low_thr_triggered;
+	int				tmp_high_thr_triggered;
+	int				tmp_low_thr_triggered;
 };
 
 struct qpnp_adc_tm_chip {
