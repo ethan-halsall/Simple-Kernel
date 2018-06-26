7b6d5f74c74936422ca3ca0d7162fd77aeba9c5f
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-27 00:28:42 +0900

    kernel: Boost all CPUs to the max when userspace launches an app
    Zygote is Android's application process, and it only forks when launching
    a new app for the first time. To accelerate app launches, boost all CPUs
    to the max for 1250 ms when zygote decides to fork.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,6 +78,7 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1960,6 +1961,10 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid))
+		cpu_input_boost_kick_max(1250);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
