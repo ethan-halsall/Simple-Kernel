3373900c1b73d6ac7a210024f82049a3548ee518
Morten Rasmussen <morten.rasmussen@arm.com>
2018-07-04 11:17:42 +0100

    UPSTREAM: sched/fair: Consider misfit tasks when load-balancing
    On asymmetric CPU capacity systems load intensive tasks can end up on
    CPUs that don't suit their compute demand.  In this scenarios 'misfit'
    tasks should be migrated to CPUs with higher compute capacity to ensure
    better throughput. group_misfit_task indicates this scenario, but tweaks
    to the load-balance code are needed to make the migrations happen.
    
    Misfit balancing only makes sense between a source group of lower
    per-CPU capacity and destination group of higher compute capacity.
    Otherwise, misfit balancing is ignored. group_misfit_task has lowest
    priority so any imbalance due to overload is dealt with first.
    
    The modifications are:
    
    1. Only pick a group containing misfit tasks as the busiest group if the
       destination group has higher capacity and has spare capacity.
    2. When the busiest group is a 'misfit' group, skip the usual average
       load and group capacity checks.
    3. Set the imbalance for 'misfit' balancing sufficiently high for a task
       to be pulled ignoring average load.
    4. Pick the CPU with the highest misfit load as the source CPU.
    5. If the misfit task is alone on the source CPU, go for active
       balancing.
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: dietmar.eggemann@arm.com
    Cc: gaku.inami.xh@renesas.com
    Cc: valentin.schneider@arm.com
    Cc: vincent.guittot@linaro.org
    Link: http://lkml.kernel.org/r/1530699470-29808-5-git-send-email-morten.rasmussen@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit cad68e552e7774b68ae6a2c5fedb792936098b72)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    
    Change-Id: If41e108f3200ade85b4a130e21eaee62bb3860fd
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8839,7 +8839,7 @@ struct lb_env {
 	unsigned int		loop_max;
 
 	enum fbq_type		fbq_type;
-	enum group_type		busiest_group_type;
+	enum group_type		src_grp_type;
 	struct list_head	tasks;
 };
 
@@ -9870,11 +9870,29 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 {
 	struct sg_lb_stats *busiest = &sds->busiest_stat;
 
+	/*
+	 * Don't try to pull misfit tasks we can't help.
+	 * We can use max_capacity here as reduction in capacity on some
+	 * CPUs in the group should either be possible to resolve
+	 * internally or be covered by avg_load imbalance (eventually).
+	 */
+	if (sgs->group_type == group_misfit_task &&
+		(!group_smaller_max_cpu_capacity(sg, sds->local) ||
+		 !group_has_capacity(env, &sds->local_stat)))
+		    return false;
+
 	if (sgs->group_type > busiest->group_type)
 		return true;
 
 	if (sgs->group_type < busiest->group_type)
 		return false;
+
+	if (sgs->avg_load <= busiest->avg_load)
+		return false;
+
+
+	if (!(env->sd->flags & SD_ASYM_CPUCAPACITY))
+		goto asym_packing;
 
 	/*
 	 * Candidate sg doesn't face any serious load-balance problems
@@ -9884,26 +9902,22 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	    !group_has_capacity(env, &sds->local_stat))
 		return false;
 
-	if (sgs->avg_load <= busiest->avg_load)
-		return false;
-
-	if (!(env->sd->flags & SD_ASYM_CPUCAPACITY))
-		goto asym_packing;
-
 	/*
 	 * Candidate sg has no more than one task per CPU and
 	 * has higher per-CPU capacity. Migrating tasks to less
 	 * capable CPUs may harm throughput. Maximize throughput,
 	 * power/energy consequences are not considered.
 	 */
+	if (sgs->sum_nr_running <= sgs->group_weight &&
+		group_smaller_min_cpu_capacity(sds->local, sg))
+		    return false;
 
 	/*
-	 * Candiate sg has no more than one task per cpu and has higher
-	 * per-cpu capacity. No reason to pull tasks to less capable cpus.
+	 * If we have more than one misfit sg go with the biggest misfit.
 	 */
-	if (sgs->sum_nr_running <= sgs->group_weight &&
-	    group_smaller_min_cpu_capacity(sds->local, sg))
-		return false;
+	if (sgs->group_type == group_misfit_task &&
+		sgs->group_misfit_task_load < busiest->group_misfit_task_load)
+			return false;
 
 asym_packing:
 	/* This is the busiest node in its class. */
@@ -10052,15 +10066,6 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 			sgs->group_no_capacity = 1;
 			sgs->group_type = group_classify(sg, sgs);
 		}
-
-		/*
-		 * Ignore task groups with misfit tasks if local group has no
-		 * capacity or if per-cpu capacity isn't higher.
-		 */
-		if (sgs->group_type == group_misfit_task && sds->local &&
-		    (!group_has_capacity(env, &sds->local_stat) ||
-		     !group_smaller_cpu_capacity(sg, sds->local)))
-			sgs->group_type = group_other;
 
 		if (update_sd_pick_busiest(env, sds, sg, sgs)) {
 			sds->busiest = sg;
@@ -10262,8 +10267,9 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 	 * factors in sg capacity and sgs with smaller group_type are
 	 * skipped when updating the busiest sg:
 	 */
-	if (busiest->avg_load <= sds->avg_load ||
-	    local->avg_load >= sds->avg_load) {
+	if (busiest->group_type != group_misfit_task &&
+		(busiest->avg_load <= sds->avg_load ||
+		 local->avg_load >= sds->avg_load)) {
 		    env->imbalance = 0;
 		    return fix_small_imbalance(env, sds);
 		}
@@ -10296,6 +10302,12 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 		max_pull * busiest->group_capacity,
 		(sds->avg_load - local->avg_load) * local->group_capacity
 	) / SCHED_CAPACITY_SCALE;
+
+	/* Boost imbalance to allow misfit task to be balanced. */
+	if (busiest->group_type == group_misfit_task) {
+		env->imbalance = max_t(long, env->imbalance,
+		       busiest->group_misfit_task_load);
+	}
 
 	/*
 	 * if *imbalance is less than the average load per runnable task
@@ -10419,8 +10431,7 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 		 * might end up to just move the imbalance on another group
 		 */
 		if ((busiest->group_type != group_overloaded) &&
-		    (local->idle_cpus <= (busiest->idle_cpus + 1)) &&
-		    !group_smaller_cpu_capacity(sds.busiest, sds.local))
+				(local->idle_cpus <= (busiest->idle_cpus + 1)))
 			goto out_balanced;
 	} else {
 		/*
@@ -10433,7 +10444,7 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 	}
 
 force_balance:
-	env->busiest_group_type = busiest->group_type;
+	env->src_grp_type = busiest->group_type;
 	/* Looks like there is an imbalance. Compute it */
 	calculate_imbalance(env, &sds);
 	return sds.busiest;
@@ -10489,19 +10500,20 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		if (rq->active_balance && rq->nr_running <= 2)
 			continue;
 
-		capacity = capacity_of(i);
 
 		/*
-		 * For ASYM_CPUCAPACITY domains, don't pick a cpu that could
-		 * eventually lead to active_balancing high->low capacity.
-		 * Higher per-cpu capacity is considered better than balancing
-		 * average load.
+		 * For ASYM_CPUCAPACITY domains with misfit tasks we simply
+		 * seek the "biggest" misfit task.
 		 */
-		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
-		    capacity_of(env->dst_cpu) < capacity &&
-		    (rq->nr_running == 1 || (rq->nr_running == 2 &&
-		     task_util(rq->curr) < sched_small_task_threshold)))
-			continue;
+		if (env->src_grp_type == group_misfit_task) {
+		    if (rq->misfit_task_load > busiest_load) {
+				busiest_load = rq->misfit_task_load;
+				busiest = rq;
+		    }
+		    continue;
+		}
+
+		capacity = capacity_of(i);
 
 		wl = weighted_cpuload(rq);
 
@@ -10511,27 +10523,8 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 */
 
 		if (rq->nr_running == 1 && wl > env->imbalance &&
-		    !check_cpu_capacity(rq, env->sd) &&
-		    env->busiest_group_type != group_misfit_task)
+		    !check_cpu_capacity(rq, env->sd))
 			continue;
-
-		/*
-		 * After enable energy awared scheduling, it has higher
-		 * priority to migrate misfit task rather than from most
-		 * loaded CPU; E.g. one CPU with single misfit task and
-		 * other CPUs with multiple lower load tasks, we should
-		 * firstly make sure the misfit task can be migrated onto
-		 * higher capacity CPU.
-		 */
-		if (energy_aware() &&
-		    capacity_orig_of(i) < capacity_orig_of(env->dst_cpu) &&
-		    rq->misfit_task &&
-		    env->busiest_group_type == group_misfit_task) {
-			busiest_load = wl;
-			busiest_capacity = capacity;
-			busiest = rq;
-			break;
-		}
 
 		/*
 		 * For the load comparisons with the other cpu's, consider
@@ -10600,6 +10593,9 @@ static int need_active_balance(struct lb_env *env)
 	    env->src_rq->misfit_task)
 		return 1;
 
+	if (env->src_grp_type == group_misfit_task)
+		return 1;
+
 	return unlikely(sd->nr_balance_failed >
 			sd->cache_nice_tries + NEED_ACTIVE_BALANCE_THRESHOLD);
 }
@@ -10648,7 +10644,7 @@ static int should_we_balance(struct lb_env *env)
 	}
 
 	if (balance_cpu == -1)
-		balance_cpu = group_balance_cpu_not_isolated(sg);
+		balance_cpu = group_balance_cpu(sg);
 
 	/*
 	 * First idle cpu or the first cpu(busiest) in this sched group
