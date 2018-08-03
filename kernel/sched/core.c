af497be82f2cd7f7fea199c9cb6786173d4eb583
Dietmar Eggemann <dietmar.eggemann@arm.com>
2018-08-03 15:05:38 +0100

    [BACKPORT] UPSTREAM: sched/fair: Remove setting task's se->runnable_weight during PELT update
    A CFS (SCHED_OTHER, SCHED_BATCH or SCHED_IDLE policy) task's
    se->runnable_weight must always be in sync with its se->load.weight.
    
    se->runnable_weight is set to se->load.weight when the task is
    forked (init_entity_runnable_average()) or reniced (reweight_entity()).
    
    There are two cases in set_load_weight() which since they currently only
    set se->load.weight could lead to a situation in which se->load.weight
    is different to se->runnable_weight for a CFS task:
    
    (1) A task switches to SCHED_IDLE.
    
    (2) A SCHED_FIFO, SCHED_RR or SCHED_DEADLINE task which has been reniced
        (during which only its static priority gets set) switches to
        SCHED_OTHER or SCHED_BATCH.
    
    Set se->runnable_weight to se->load.weight in these two cases to prevent
    this. This eliminates the need to explicitly set it to se->load.weight
    during PELT updates in the CFS scheduler fastpath.
    
    Bug: 120440300
    Change-Id: I52184a9e1fd53cb42ef3ae546b1fae78b744c9ad
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Patrick Bellasi <patrick.bellasi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Quentin Perret <quentin.perret@arm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Link: http://lkml.kernel.org/r/20180803140538.1178-1-dietmar.eggemann@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 4a465e3ebbc8004ce4f7f08f6022ee8315a94edf)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -764,6 +764,7 @@ static void set_load_weight(struct task_struct *p, bool update_load)
 	if (idle_policy(p->policy)) {
 		load->weight = scale_load(WEIGHT_IDLEPRIO);
 		load->inv_weight = WMULT_IDLEPRIO;
+		p->se.runnable_weight = load->weight;
 		return;
 	}
 
@@ -776,6 +777,7 @@ static void set_load_weight(struct task_struct *p, bool update_load)
 	} else {
 		load->weight = scale_load(sched_prio_to_weight[prio]);
 		load->inv_weight = sched_prio_to_wmult[prio];
+		p->se.runnable_weight = load->weight;
 	}
 }
 
