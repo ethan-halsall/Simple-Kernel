198af75e98c4e2e1c57544be852564b505b76cf6
Minchan Kim <minchan@google.com>
2018-08-06 15:12:44 +0900

    mm: mm_event: add special kernel allocation stat
    Record the count of special page allocation on the process context.
    
    This patch aims for accounting of special page allocation which
    consumed a lot by android system.
    At this moment, ION system heap is good candidate(it could cover
    other kernel allocation in future).
    With that, we could keep tracking burst kernel allocation owner
    so that it would be useful to find places caused by lmk, reclaim,
    compaction latency.
    
    Bug: 80168800
    Change-Id: I5942fd940d98baa2eb814f66b076cb37ecd3b4aa
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -41,6 +41,7 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 			goto error_free_pages;
 
 	ion_page_pool_alloc_set_cache_policy(pool, page);
+	mm_event_count(MM_KERN_ALLOC, 1 << pool->order);
 	mod_node_page_state(page_pgdat(page), NR_ION_HEAP,
 				1 << pool->order);
 
