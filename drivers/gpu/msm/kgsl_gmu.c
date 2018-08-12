a6c8a52c37f64771ceb9f95eb6c4666e907b218f
Sultan Alsawaf <sultanxda@gmail.com>
2018-08-11 19:57:09 -0700

    msm: kgsl: Remove POPP
    POPP constantly attempts to lower the GPU's frequency behind the governor's
    back in order to save power; however, the GPU governor in use
    (msm-adreno-tz) is very good at determining the GPU's load and selecting
    an appropriate frequency to run the GPU at.
    
    POPP was created long ago (perhaps when msm-adreno-tz didn't exist or
    didn't work so well), so it is clearly deprecated. Remove it.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/gpu/msm/kgsl_gmu.c
+++ drivers/gpu/msm/kgsl_gmu.c
@@ -1420,7 +1420,7 @@ int gmu_start(struct kgsl_device *device)
 
 		/* Vote for 300MHz DDR for GMU to init */
 		ret = msm_bus_scale_client_update_request(gmu->pcl,
-				pwr->pwrlevels[pwr->default_pwrlevel].bus_freq);
+				pwr->pwrlevels[pwr->num_pwrlevels - 1].bus_freq);
 		if (ret)
 			dev_err(&gmu->pdev->dev,
 				"Failed to allocate gmu b/w: %d\n", ret);
@@ -1435,7 +1435,7 @@ int gmu_start(struct kgsl_device *device)
 			goto error_gmu;
 
 		/* Request default DCVS level */
-		gmu_change_gpu_pwrlevel(device, pwr->default_pwrlevel);
+		gmu_change_gpu_pwrlevel(device, pwr->num_pwrlevels - 1);
 		msm_bus_scale_client_update_request(gmu->pcl, 0);
 		break;
 
@@ -1463,7 +1463,7 @@ int gmu_start(struct kgsl_device *device)
 		if (ret)
 			goto error_gmu;
 
-		gmu_change_gpu_pwrlevel(device, pwr->default_pwrlevel);
+		gmu_change_gpu_pwrlevel(device, pwr->num_pwrlevels - 1);
 		break;
 
 	case KGSL_STATE_RESET:
@@ -1486,7 +1486,7 @@ int gmu_start(struct kgsl_device *device)
 
 			/* Send DCVS level prior to reset*/
 			gmu_change_gpu_pwrlevel(device,
-				pwr->default_pwrlevel);
+				pwr->num_pwrlevels - 1);
 		} else {
 			/* GMU fast boot */
 			hfi_stop(gmu);
