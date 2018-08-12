93d65707484489b32f536152310ce9809e1638b7
Sultan Alsawaf <sultanxda@gmail.com>
2018-08-11 20:02:36 -0700

    kernel: Boost cpubw devfreq device to the max for app launches
    Speeds up cold app launches.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -79,6 +79,7 @@
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
 #include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1963,8 +1964,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost CPU to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
+	if (is_zygote_pid(current->pid)) {
 		cpu_input_boost_kick_max(1250);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
