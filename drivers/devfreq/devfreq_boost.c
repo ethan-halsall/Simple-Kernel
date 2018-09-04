6495bd457ef87af97986d9ce879a2d6d31207586
Erik Müller <erik.lars.mueller@googlemail.com>
2018-09-04 23:59:19 +0200

    devfreq_boost: Convert to msm_drm_notifier
    According to logging, these statements are never being hit and this
    matches what OnePlus has done in the Synaptics driver.
    
    [nc: Reworded title and added commit message]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -16,6 +16,7 @@
 #include <linux/devfreq_boost.h>
 #include <linux/fb.h>
 #include <linux/input.h>
+#include <linux/msm_drm_notify.h>
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
@@ -252,16 +253,16 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	unsigned long action, void *data)
 {
 	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
-	struct fb_event *evdata = data;
-	int *blank = evdata->data;
+	struct msm_drm_notifier *evdata = data;
+	int blank = *(int *)evdata->data;
 	bool screen_awake;
 
 	/* Parse framebuffer blank events as soon as they occur */
-	if (action != FB_EARLY_EVENT_BLANK)
+	if (action != MSM_DRM_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	screen_awake = *blank == FB_BLANK_UNBLANK;
+	screen_awake = blank == MSM_DRM_BLANK_UNBLANK_CUST;
 	devfreq_disable_boosting(d, !screen_awake);
 	if (screen_awake) {
 		int i;
@@ -398,7 +399,7 @@ static int __init devfreq_boost_init(void)
 
 	d->fb_notif.notifier_call = fb_notifier_cb;
 	d->fb_notif.priority = INT_MAX;
-	ret = fb_register_client(&d->fb_notif);
+	ret = msm_drm_register_client(&d->fb_notif);
 	if (ret) {
 		pr_err("Failed to register fb notifier, err: %d\n", ret);
 		goto unregister_handler;
