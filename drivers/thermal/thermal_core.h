38967ad97694107050a5afcf9132987c6eef2048
Ram Chandrasekar <rkumbako@codeaurora.org>
2018-11-02 12:23:40 -0600

    drivers: thermal: Evaluate based on trip temperature
    With virtual sensors, temperature reads by different zones at different
    time can be different and hence can result in different set of
    thresholds. This can lead to a case where we can get interrupt for the
    same temperature back to back if the temperature is fluctuating.
    
    To avoid this, allow sensor drivers to read the temperature and provide
    that information along when notifying for trip notification. Thermal
    framework will now make sure all the thermal zones will use the same
    temperature in all thermal zones to evaluate.
    
    Change-Id: I10861be54f0948081a83a1e839d8f9401929c61b
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -70,7 +70,6 @@ extern struct thermal_message *tm;
 
 int thermal_register_governor(struct thermal_governor *);
 void thermal_unregister_governor(struct thermal_governor *);
-void handle_thermal_trip(struct thermal_zone_device *tz, int trip);
 
 #ifdef CONFIG_THERMAL_GOV_STEP_WISE
 int thermal_gov_step_wise_register(void);
@@ -134,6 +133,8 @@ int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 void of_thermal_handle_trip(struct thermal_zone_device *tz);
 int of_parse_thermal_message(void);
 void free_thermal_message(void);
+void of_thermal_handle_trip_temp(struct thermal_zone_device *tz,
+					int trip_temp);
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
@@ -159,6 +160,10 @@ static inline int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 }
 static inline
 void of_thermal_handle_trip(struct thermal_zone_device *tz)
+{ }
+static inline
+void of_thermal_handle_trip_temp(struct thermal_zone_device *tz,
+					int trip_temp)
 { }
 #endif
 
