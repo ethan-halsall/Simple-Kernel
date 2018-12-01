046ff20f6557086dd634787a12c2cd3e72b721d0
Francisco Franco <franciscofranco.1990@gmail.com>
2018-12-01 16:50:10 +0000

    kernel: keep the cpumasks for cpu_input_boost
    From https://github.com/franciscofranco/beryllium/commit/00f07f7e08d353446c7cae6e402eb9bdad3831f5
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -33,6 +33,20 @@
 
 #include "smpboot.h"
 
+/*
+ * This assumes that half of the CPUs are little and that they have lower
+ * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
+ * little and CPUs 4-7 would be big).
+ */
+#define LITTLE_CPU_MASK	((1UL << (NR_CPUS / 2)) - 1)
+#define BIG_CPU_MASK	(((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
+static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
+const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
+EXPORT_SYMBOL(cpu_lp_mask);
+static const unsigned long big_cluster_cpus = BIG_CPU_MASK;
+const struct cpumask *const cpu_perf_mask = to_cpumask(&big_cluster_cpus);
+EXPORT_SYMBOL(cpu_perf_mask);
+
 /**
  * cpuhp_cpu_state - Per cpu hotplug state storage
  * @state:	The current cpu state
