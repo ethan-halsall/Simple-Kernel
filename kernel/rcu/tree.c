6c0b19afb331b1f145e422682ae03cce5f24219a
Akira Yokosawa <akiyks@gmail.com>
2019-03-02 17:25:19 +0900

    rcu: Move common code out of if-else block
    As the result of recent addition of "rdp->core_needs_qs = false;" in
    the "if" block, now both branches of the if-else have the same
    assignment.
    
    Factor it out and reduce line count.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>
    Acked-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2303,7 +2303,6 @@ rcu_report_qs_rdp(int cpu, struct rcu_data *rdp)
 	mask = rdp->grpmask;
 	rdp->core_needs_qs = false;
 	if ((rnp->qsmask & mask) == 0) {
-		rdp->core_needs_qs = false;
 		raw_spin_unlock_irqrestore_rcu_node(rnp, flags);
 	} else {
 		/*
