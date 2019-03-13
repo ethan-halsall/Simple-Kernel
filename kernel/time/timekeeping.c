1562bfdadee8a2d544bf35263272d3e44919aa14
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-13 13:21:09 +0900

    timekeeping: offset printk time upon resuming from suspend
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1675,6 +1675,9 @@ void timekeeping_inject_sleeptime64(struct timespec64 *delta)
 
 	/* signal hrtimers about time change */
 	clock_was_set();
+
+	/* Offset printk time */
+	set_printk_offset(ktime_to_ns(tk->offs_boot));
 }
 #endif
 
