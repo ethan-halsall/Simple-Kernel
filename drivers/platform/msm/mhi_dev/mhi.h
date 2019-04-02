4125407f2b730ba81de01d1554a4bf4ca407761a
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-02 15:11:29 +0900

    treewide: make every ipc_loggings a no-op
    The compiler will now delete ipc_loggings completely.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/platform/msm/mhi_dev/mhi.h
+++ drivers/platform/msm/mhi_dev/mhi.h
@@ -595,10 +595,6 @@ extern void *mhi_ipc_log;
 	if (_msg_lvl >= mhi_msg_lvl) { \
 		pr_err("[%s] "_msg, __func__, ##__VA_ARGS__); \
 	} \
-	if (mhi_ipc_log && (_msg_lvl >= mhi_ipc_msg_lvl)) { \
-		ipc_log_string(mhi_ipc_log,                     \
-			"[%s] " _msg, __func__, ##__VA_ARGS__);     \
-	} \
 } while (0)
 
 
