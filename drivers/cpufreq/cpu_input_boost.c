36bec7593f9664c8084f2944cee8e66b96feebf1
Danny Lin <danny@kdrag0n.dev>
2019-04-03 18:28:33 -0700

    cpu_input_boost: Allow configuration of each cluster's minimum frequency
    Based on kdrag0n/proton_bluecross@64e4be6:
    
    On some CPUs, it can be more efficient to use a higher base frequency
    after all boosts have expired. For example, the low-power cluster on
    sdm845 benefits from using a minimum frequency of 576000 KHz over 300000
    KHz because it uses ~6% more power while offering nearly 2x the
    performance. This is the minimum frequency used by Google, Qualcomm, and
    OnePlus for good reason. It is actually possible for battery life to
    improve when using this elevated base frequency because there are less
    CPU frequency transitions (which are relatively expensive) and less
    "ping-pong" effects where the CPU constantly switches between
    frequencies in the 300000 KHz - 576000 KHz range. This has been observed
    to occur during video playback in some cases.
    
    This can also be used to bump the performance baseline for intensive
    tasks such as gaming.
    
    Co-authored-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Co-authored-by: Nathan Chancellor <natechancellor@gmail.com>
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    [@0ctobot: Synchronize parameter naming conventions with c1c82af
    ("cpu_input_boost: add an option to choose a custom boost finish return
    freq for the LP cluster") and 6fa4340 ("cpu_input_boost: Add ability to
    choose minimum frequency for big cluster")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -27,6 +27,10 @@ static unsigned int max_boost_freq_lp __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_LP;
 static unsigned int max_boost_freq_hp __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_PERF;
+static unsigned int remove_input_boost_freq_lp __read_mostly =
+	CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
+static unsigned int remove_input_boost_freq_perf __read_mostly =
+	CONFIG_REMOVE_INPUT_BOOST_FREQ_PERF;
 
 static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
@@ -37,6 +41,8 @@ module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
 module_param(max_boost_freq_lp, uint, 0644);
 module_param(max_boost_freq_hp, uint, 0644);
+module_param(remove_input_boost_freq_lp, uint, 0644);
+module_param(remove_input_boost_freq_perf, uint, 0644);
 
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
@@ -80,6 +86,18 @@ static u32 get_max_boost_freq(struct cpufreq_policy *policy)
 		freq = max_boost_freq_hp;
 
 	return min(freq, policy->max);
+}
+
+static u32 get_min_freq(struct cpufreq_policy *policy)
+{
+	u32 freq;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = remove_input_boost_freq_lp;
+	else
+		freq = remove_input_boost_freq_perf;
+
+	return max(freq, policy->cpuinfo.min_freq);
 }
 
 static u32 get_boost_state(struct boost_drv *b)
@@ -224,7 +242,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 
 	/* Unboost when the screen is off */
 	if (state & SCREEN_OFF) {
-		policy->min = policy->cpuinfo.min_freq;
+		policy->min = get_min_freq(policy);
 		return NOTIFY_OK;
 	}
 
@@ -241,7 +259,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	if (state & INPUT_BOOST)
 		policy->min = get_input_boost_freq(policy);
 	else
-		policy->min = policy->cpuinfo.min_freq;
+		policy->min = get_min_freq(policy);
 
 	return NOTIFY_OK;
 }
