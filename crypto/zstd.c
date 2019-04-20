38516286d815c241d1b2a1b23db01251fd971784
DevriesL <therkduan@gmail.com>
2019-04-20 22:33:12 +0800

    zstd: merge optimization from Samsung
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -23,7 +23,7 @@
 #include <crypto/internal/scompress.h>
 
 
-#define ZSTD_DEF_LEVEL	3
+#define ZSTD_DEF_LEVEL	1
 
 struct zstd_ctx {
 	ZSTD_CCtx *cctx;
@@ -34,7 +34,7 @@ struct zstd_ctx {
 
 static ZSTD_parameters zstd_params(void)
 {
-	return ZSTD_getParams(ZSTD_DEF_LEVEL, 0, 0);
+	return ZSTD_getParams(ZSTD_DEF_LEVEL, PAGE_SIZE, 0);
 }
 
 static int zstd_comp_init(struct zstd_ctx *ctx)
