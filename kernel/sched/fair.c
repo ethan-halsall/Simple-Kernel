bd26af05aeeeeefe800f4f36a94dacd33729786a
Valentin Schneider <valentin.schneider@arm.com>
2019-04-25 12:05:40 +0100

    sched/fair: Also do reduced-capacity active balance when !idle
    In
    
      commit 1aaf90a4b88a ("sched: Move CFS tasks to CPUs with higher capacity")
    
    was introduced an active balance condition that caters to migrating
    CFS tasks away from heavily pressured CPUs.
    
    This active balance scenario only happens for !CPU_NOT_IDLE scenarios,
    IOW idle or newidle balances. However, it can still make sense to do
    it when the destination CPU is not idle, providing we can give more
    compute-capacity to the targeted task.
    
    Tweak the condition to also do active balance when dst_cpu is not idle
    but has more spare capacity than the pressured src_cpu
    
    Change-Id: I1f7facdb297cabc8119d30317435de2a92797b13
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10257,17 +10257,15 @@ static int need_active_balance(struct lb_env *env)
 	}
 
 	/*
-	 * The dst_cpu is idle and the src_cpu CPU has only 1 CFS task.
-	 * It's worth migrating the task if the src_cpu's capacity is reduced
-	 * because of other sched_class or IRQs if more capacity stays
-	 * available on dst_cpu.
+	 * src_cpu has only 1 CFS task. If its capacity is significantly
+	 * reduced (RT/IRQ pressure), it's worth moving that task if dst_cpu
+	 * can provide more capacity.
 	 */
-	if ((env->idle != CPU_NOT_IDLE) &&
-	    (env->src_rq->cfs.h_nr_running == 1)) {
-		if ((check_cpu_capacity(env->src_rq, sd)) &&
-		    (capacity_of(env->src_cpu)*sd->imbalance_pct < capacity_of(env->dst_cpu)*100))
-			return 1;
-	}
+	if (env->src_rq->cfs.h_nr_running == 1 &&
+	    check_cpu_capacity(env->src_rq, sd) &&
+	    (capacity_of(env->dst_cpu) - cpu_util(env->dst_cpu)) * 100 >
+	    capacity_of(env->src_cpu) * sd->imbalance_pct)
+		return 1;
 
 	if ((env->idle != CPU_NOT_IDLE) &&
 	    (capacity_orig_of(env->src_cpu) < capacity_orig_of(env->dst_cpu)) &&
