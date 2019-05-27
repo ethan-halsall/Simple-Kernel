8695778bd580504d6a0e21488a4083f2a2d85971
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-27 16:48:38 -0700

    kernel: Allow IRQs to become perf-critical after they're created
    This provides an API (irq_set_perf_affinity) to make an active IRQ
    performance critical.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -211,6 +211,7 @@ extern void enable_irq(unsigned int irq);
 extern void enable_percpu_irq(unsigned int irq, unsigned int type);
 extern bool irq_percpu_is_enabled(unsigned int irq);
 extern void irq_wake_thread(unsigned int irq, void *dev_id);
+extern void irq_set_perf_affinity(unsigned int irq);
 
 /* The following three functions are for the core kernel use only. */
 extern void suspend_device_irqs(void);
