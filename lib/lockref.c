410b607cae8edcdcc604c0f879951b75468c6934
Jan Glauber <jglauber@cavium.com>
2019-06-05 15:48:49 +0200

    lockref: Limit number of cmpxchg loop retries
    The lockref cmpxchg loop is unbound as long as the spinlock is not
    taken. Depending on the hardware implementation of compare-and-swap
    a high number of loop retries might happen.
    
    Add an upper bound to the loop to force the fallback to spinlocks
    after some time. A retry value of 100 should not impact any hardware
    that does not have this issue.
    
    With the retry limit the performance of an open-close testcase
    improved between 60-70% on ThunderX2.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jan Glauber <jglauber@marvell.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- lib/lockref.c
+++ lib/lockref.c
@@ -8,6 +8,7 @@
  * failure case.
  */
 #define CMPXCHG_LOOP(CODE, SUCCESS) do {					\
+	int retry = 100;							\
 	struct lockref old;							\
 	BUILD_BUG_ON(sizeof(old) != 8);						\
 	old.lock_count = READ_ONCE(lockref->lock_count);			\
@@ -20,6 +21,8 @@
 		if (likely(old.lock_count == prev.lock_count)) {		\
 			SUCCESS;						\
 		}								\
+		if (!--retry)							\
+			break;							\
 		cpu_relax();							\
 	}									\
 } while (0)
