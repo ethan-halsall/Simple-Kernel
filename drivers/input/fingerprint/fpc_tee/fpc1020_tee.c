6111c84d798c201df14e907d7f1c675e9c9442ec
ethan-halsall <ethan.halsall@gmail.com>
2019-06-09 10:27:27 +0100

    fingerprint: fpc_tee: Fix duplicate declaration specifier warnings
    Change-Id: I6bc9752bbe6a27dde4b66b950f9b596bd6e1eeb0
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -68,7 +68,7 @@ struct vreg_config {
 	int ua_load;
 };
 
-static const struct vreg_config const vreg_conf[] = {
+static const struct vreg_config vreg_conf[] = {
 	{ "vdd_ana", 1800000UL, 1800000UL, 6000, },
 	{ "fp_vdd_vreg", 3000000UL, 3000000UL, 6000, },
 	/*{ "vcc_spi", 1800000UL, 1800000UL, 10, },*/
