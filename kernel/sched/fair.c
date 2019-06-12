09f4285d3311ed324d2dad07ce41f5e2ab51a56f
Zachariah Kennedy <zkennedy87@gmail.com>
2019-06-12 16:20:06 +0000

    sched/fair: Be more aggressive preventing downmigration of Schedtune boosted tasks
    The first iteration of this patch would apply the flag
    LBF_IGNORE_BIG_TASKS to tasks being looked at for downmigration.
    Then when detach_tasks would call can_migrate_task() and it would
    make sure the source runqueue was not overutilized, had
    LBF_IGNORE_BIG_TASKS set, and was Schedtune boosted.
    But detach_tasks() runs in a loop and if no tasks would be picked,
    the flag LBF_IGNORE_BIG_TASKS would be removed and the loop would
    start back over. Now, the Schedtune boosted tasks would be
    downmigrated.
    
    Now we just check within can_migrate_task() if the tasks is
    not overutilized, moving from a runqueue with larger capacity than
    the destination, and is Schedtune boosted.
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Michael <loukerismichalis@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8701,7 +8701,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	 * big cores are overutilized
 	 */
 	if (!env->src_rq->rd->overutilized &&
-		env->flags & LBF_IGNORE_BIG_TASKS &&
+		(cpu_capacity(env->dst_cpu) < cpu_capacity(env->src_cpu)) &&
 		(schedtune_task_boost(p) > 0))
 		return 0;
 
