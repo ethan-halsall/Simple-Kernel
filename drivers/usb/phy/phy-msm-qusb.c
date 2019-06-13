c31c01033f42c72b456edf37c85d72876741d68f
ethan-halsall <ethan.halsall@gmail.com>
2019-06-13 22:47:26 +0100

    drivers/usb/phy: fix misleading indentation
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -1039,7 +1039,7 @@ static int qusb_phy_probe(struct platform_device *pdev)
 			qphy->iface_clk = NULL;
 		if (ret == -EPROBE_DEFER)
 			return ret;
-			dev_err(dev, "couldn't get iface_clk(%d)\n", ret);
+		dev_err(dev, "couldn't get iface_clk(%d)\n", ret);
 		}
 	}
 
