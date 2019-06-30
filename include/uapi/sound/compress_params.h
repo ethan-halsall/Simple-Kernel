1813400fb5f81ab8c5cb2fcc79e22001a79b3a35
Asish Bhattacharya <asishb@codeaurora.org>
2019-06-30 20:00:32 +0530

    ASoC: msm: qdsp6v2: add support for AMR_WB_PLUS offload
    Add AMR_WB_PLUS to supported offload formats and send media
    format block through compress driver.
    
    Change-Id: I258dc05fd5b6442ef40a5d1d4538870c4ddcbe2e
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -409,14 +409,9 @@ struct snd_dec_aptx {
 	__u32 nap;
 };
 
-/** struct snd_dec_pcm - codec options for PCM format
- * @num_channels: Number of channels
- * @ch_map: Channel map for the above corresponding channels
- */
-struct snd_dec_pcm {
-	__u32 num_channels;
-	__u8 ch_map[MAX_PCM_DECODE_CHANNELS];
-} __attribute__((packed, aligned(4)));
+struct snd_dec_amrwb_plus {
+	__u32 bit_stream_fmt;
+};
 
 union snd_codec_options {
 	struct snd_enc_wma wma;
@@ -430,7 +425,7 @@ union snd_codec_options {
 	struct snd_dec_alac alac;
 	struct snd_dec_ape ape;
 	struct snd_dec_aptx aptx_dec;
-	struct snd_dec_pcm pcm_dec;
+	struct snd_dec_amrwb_plus amrwbplus;
 };
 
 /** struct snd_codec_desc - description of codec capabilities
