24bd88f116469beedab3cb999b71f9f493c74210
ethan-halsall <ethan.halsall@gmail.com>
2019-08-01 15:55:49 +0100

    Revert "cpufreq: schedutil: Cleanup sugov_get_util"
    This reverts commit 2a2d0550aa54644311b4c9a7f591ab693cc8c145.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -211,9 +211,15 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 {
+	struct rq *rq = cpu_rq(cpu);
+	unsigned long cfs_max;
 	struct sugov_cpu *loadcpu = &per_cpu(sugov_cpu, cpu);
 
-	*max = arch_scale_cpu_capacity(NULL, cpu);
+	cfs_max = arch_scale_cpu_capacity(NULL, cpu);
+
+	*util = min(rq->cfs.avg.util_avg, cfs_max);
+	*max = cfs_max;
+
 	*util = boosted_cpu_util(cpu, &loadcpu->walt_load);
 }
 
