bda7942ef1f6e3e4d98eeffc522712ec525347a0
ethan-halsall <ethan.halsall@gmail.com>
2019-08-02 15:22:35 +0100

    init.simple.sh: disable caf load tracking
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- zip-out/ramdisk/init.simple.sh
+++ zip-out/ramdisk/init.simple.sh
@@ -24,6 +24,12 @@ done;
 # Disable CAF task placement for Big Cores
 echo 0 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
+# configure governor settings for little cluster
+echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
+	
+# configure governor settings for big cluster
+echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/pl
+
 # Setup EAS cpusets values for better load balancing
 echo "0-7" > /dev/cpuset/top-app/cpus 
 
