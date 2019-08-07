956a54b8767c6d9652d7f790904754e892f5ceb2
ethan-halsall <ethan.halsall@gmail.com>
2019-08-07 22:01:19 +0100

    f2fs: remove duplicate kvmalloc definition
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2542,15 +2542,6 @@ static inline void *f2fs_kmalloc(struct f2fs_sb_info *sbi,
 	return kmalloc(size, flags);
 }
 
-static inline void *kvmalloc(size_t size, gfp_t flags)
-{
-	void *ret;
-
-	ret = kmalloc(size, flags | __GFP_NOWARN);
-	if (!ret)
-		ret = __vmalloc(size, flags, PAGE_KERNEL);
-	return ret;
-}
 
 static inline int wbc_to_write_flags(struct writeback_control *wbc)
 {
