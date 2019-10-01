80a2a142ffd97ee50a04092ed36714696f513d99
Arun Prakash <app@codeaurora.org>
2019-10-01 17:13:40 +0530

    diag: Preventing deadlock by adding spin_unlock
    Adding spin_unlock_irqrestore in mhi_write to avoid deadlock.
    
    Change-Id: If1525d46066a2bcb49e764852034f39a1419eb44
    Signed-off-by: Arun Prakash <app@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/char/diag/diagfwd_mhi.c
+++ drivers/char/diag/diagfwd_mhi.c
@@ -511,8 +511,10 @@ static int mhi_write(int id, unsigned char *buf, int len, int ctxt)
 
 	err = mhi_buf_tbl_add(&diag_mhi[id], TYPE_MHI_WRITE_CH, buf,
 			      len);
-	if (err)
+	if (err) {
+		spin_unlock_irqrestore(&ch->lock, flags);
 		goto fail;
+	}
 
 	spin_lock_irqsave(&ch->lock, flags);
 	err = mhi_queue_xfer(ch->hdl, buf, len, mhi_flags);
