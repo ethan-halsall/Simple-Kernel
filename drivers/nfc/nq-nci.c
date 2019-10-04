f1627288e0d05bdce595c1e068625d38fb4979bc
YumeMichi <do4suki@gmail.com>
2019-10-04 16:37:49 +0800

    NFC: Fix -Wunused-function warning
    ../drivers/nfc/nq-nci.c:156:12: warning: unused function 'is_data_available_for_read' [-Wunused-function]
    static int is_data_available_for_read(struct nqx_dev *nqx_dev)
               ^
    
    Change-Id: Ie845a8278c2c6255efbc75032b1c0818df37696d
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- drivers/nfc/nq-nci.c
+++ drivers/nfc/nq-nci.c
@@ -152,7 +152,7 @@ static irqreturn_t nqx_dev_irq_handler(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static int is_data_available_for_read(struct nqx_dev *nqx_dev)
+static int __maybe_unused is_data_available_for_read(struct nqx_dev *nqx_dev)
 {
 	int ret;
 
