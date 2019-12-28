b194c6089f308136498008560a65d19099a094da
Ethan Halsall <ethan.halsall@gmail.com>
2019-12-28 09:31:15 +0000

    Disbale SchedTune utilization boosting
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -114,4 +114,4 @@ SCHED_FEAT(EAS_USE_NEED_IDLE, true)
  * generally leads to usage of higher frequencies.
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
-SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, true)
+SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
