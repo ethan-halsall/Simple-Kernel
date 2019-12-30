97c5ea4c906f3ed369796b940316a444edb19eb5
DennySPB <dennyspb@gmail.com>
2019-12-30 13:39:04 +0300

    Remove nodeloopthread from stune blacklist
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -128,7 +128,7 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+	return !strcmp(comm, "init") ||
 	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
