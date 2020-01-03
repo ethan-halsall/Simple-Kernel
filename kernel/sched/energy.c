3e9a2a07c4a9997d58b2f3bf7bda0915fb40dba8
ethan-halsall <ethan.halsall@gmail.com>
2020-01-03 20:14:46 +0000

    Revert "sched/energy: Check out to Android 4.14-q common kernel"
    This reverts commit bc5781fba5d7e355f8925c744861d64affcae128.

--- kernel/sched/energy.c
+++ kernel/sched/energy.c
@@ -28,6 +28,7 @@
 #include <linux/cpuset.h>
 #include <linux/pm_opp.h>
 #include <linux/platform_device.h>
+
 #include "sched.h"
 
 struct sched_group_energy *sge_array[NR_CPUS][NR_SD_LEVELS];
@@ -49,20 +50,16 @@ static void free_resources(void)
 	}
 }
 static bool sge_ready;
-static bool freq_energy_model;
 
-void check_max_cap_vs_cpu_scale(int cpu, struct sched_group_energy *sge)
+static int update_topology;
+
+/*
+ * Ideally this should be arch specific implementation,
+ * let's define here to help rebuild sched_domain with new capacities.
+ */
+int arch_update_cpu_topology(void)
 {
-	unsigned long max_cap, cpu_scale;
-
-	max_cap = sge->cap_states[sge->nr_cap_states - 1].cap;
-	cpu_scale = scale_cpu_capacity(NULL, cpu);
-
-	if (max_cap == cpu_scale)
-		return;
-
-	pr_warn("CPU%d max energy model capacity=%ld != cpu_scale=%ld\n", cpu,
-		max_cap, cpu_scale);
+	return update_topology;
 }
 
 void init_sched_energy_costs(void)
@@ -75,6 +72,9 @@ void init_sched_energy_costs(void)
 	int sd_level, i, nstates, cpu;
 	const __be32 *val;
 
+	if (!sched_is_energy_aware())
+		return;
+
 	for_each_possible_cpu(cpu) {
 		cn = of_get_cpu_node(cpu, NULL);
 		if (!cn) {
@@ -86,9 +86,6 @@ void init_sched_energy_costs(void)
 			pr_warn("CPU device node has no sched-energy-costs\n");
 			return;
 		}
-		/* Check if the energy model contains frequency/power values */
-		if (of_find_property(cn, "freq-energy-model", NULL))
-			freq_energy_model = true;
 
 		for_each_possible_sd_level(sd_level) {
 			cp = of_parse_phandle(cn, "sched-energy-costs", sd_level);
@@ -103,25 +100,21 @@ void init_sched_energy_costs(void)
 
 			sge = kcalloc(1, sizeof(struct sched_group_energy),
 				      GFP_NOWAIT);
+			if (!sge)
+				goto out;
 
 			nstates = (prop->length / sizeof(u32)) / 2;
 			cap_states = kcalloc(nstates,
 					     sizeof(struct capacity_state),
 					     GFP_NOWAIT);
+			if (!cap_states) {
+				kfree(sge);
+				goto out;
+			}
 
 			for (i = 0, val = prop->value; i < nstates; i++) {
-				if (freq_energy_model) {
-					/*
-					 * Capacity values will be calculated later using
-					 * frequency reported by OPP driver and cpu_uarch_scale
-					 * values.
-					 */
-					cap_states[i].frequency = be32_to_cpup(val++);
-					cap_states[i].cap = 0;
-				} else {
-					cap_states[i].frequency = 0;
-					cap_states[i].cap = be32_to_cpup(val++);
-				}
+				cap_states[i].cap = SCHED_CAPACITY_SCALE;
+				cap_states[i].frequency = be32_to_cpup(val++);
 				cap_states[i].power = be32_to_cpup(val++);
 			}
 
@@ -131,6 +124,8 @@ void init_sched_energy_costs(void)
 			prop = of_find_property(cp, "idle-cost-data", NULL);
 			if (!prop || !prop->value) {
 				pr_warn("No idle-cost data, skipping sched_energy init\n");
+				kfree(sge);
+				kfree(cap_states);
 				goto out;
 			}
 
@@ -138,6 +133,11 @@ void init_sched_energy_costs(void)
 			idle_states = kcalloc(nstates,
 					      sizeof(struct idle_state),
 					      GFP_NOWAIT);
+			if (!idle_states) {
+				kfree(sge);
+				kfree(cap_states);
+				goto out;
+			}
 
 			for (i = 0, val = prop->value; i < nstates; i++)
 				idle_states[i].power = be32_to_cpup(val++);
@@ -147,9 +147,8 @@ void init_sched_energy_costs(void)
 
 			sge_array[cpu][sd_level] = sge;
 		}
-		if (!freq_energy_model)
-			check_max_cap_vs_cpu_scale(cpu, sge_array[cpu][SD_LEVEL0]);
 	}
+
 	sge_ready = true;
 	pr_info("Sched-energy-costs installed from DT\n");
 	return;
@@ -160,15 +159,17 @@ out:
 
 static int sched_energy_probe(struct platform_device *pdev)
 {
+	unsigned long max_freq = 0;
+	int max_efficiency = INT_MIN;
 	int cpu;
 	unsigned long *max_frequencies = NULL;
 	int ret;
+	bool is_sge_valid = false;
 
+	if (!sched_is_energy_aware())
+		return 0;
 	if (!sge_ready)
 		return -EPROBE_DEFER;
-
-	if (!energy_aware() || !freq_energy_model)
-		return 0;
 
 	max_frequencies = kmalloc_array(nr_cpu_ids, sizeof(unsigned long),
 					GFP_KERNEL);
@@ -184,6 +185,9 @@ static int sched_energy_probe(struct platform_device *pdev)
 	for_each_possible_cpu(cpu) {
 		struct device *cpu_dev;
 		struct dev_pm_opp *opp;
+		int efficiency = arch_get_cpu_efficiency(cpu);
+
+		max_efficiency = max(efficiency, max_efficiency);
 
 		cpu_dev = get_cpu_device(cpu);
 		if (IS_ERR_OR_NULL(cpu_dev)) {
@@ -196,6 +200,7 @@ static int sched_energy_probe(struct platform_device *pdev)
 
 		max_frequencies[cpu] = ULONG_MAX;
 
+		rcu_read_lock();
 		opp = dev_pm_opp_find_freq_floor(cpu_dev,
 						 &max_frequencies[cpu]);
 		if (IS_ERR_OR_NULL(opp)) {
@@ -203,18 +208,25 @@ static int sched_energy_probe(struct platform_device *pdev)
 				ret = -EPROBE_DEFER;
 			else
 				ret = PTR_ERR(opp);
-			goto exit;
+			goto exit_rcu_unlock;
 		}
+		rcu_read_unlock();
 
 		/* Convert HZ to KHZ */
 		max_frequencies[cpu] /= 1000;
+		max_freq = max(max_freq, max_frequencies[cpu]);
 	}
 
 	/* update capacity in energy model */
 	for_each_possible_cpu(cpu) {
 		unsigned long cpu_max_cap;
 		struct sched_group_energy *sge_l0, *sge;
-		cpu_max_cap = scale_cpu_capacity(NULL, cpu);
+		int efficiency = arch_get_cpu_efficiency(cpu);
+
+		cpu_max_cap = DIV_ROUND_UP(SCHED_CAPACITY_SCALE *
+					   max_frequencies[cpu], max_freq);
+		cpu_max_cap = DIV_ROUND_UP(cpu_max_cap * efficiency,
+					   max_efficiency);
 
 		/*
 		 * All the cap_states have same frequency table so use
@@ -246,15 +258,17 @@ static int sched_energy_probe(struct platform_device *pdev)
 						break;
 					sge->cap_states[i].cap = cap;
 				}
+
 				dev_dbg(&pdev->dev,
 					"cpu=%d freq=%ld cap=%ld power_d0=%ld\n",
 					cpu, freq, sge_l0->cap_states[i].cap,
 					sge_l0->cap_states[i].power);
 			}
 
+			is_sge_valid = true;
 			dev_info(&pdev->dev,
-				"cpu=%d [freq=%ld cap=%ld power_d0=%ld] -> [freq=%ld cap=%ld power_d0=%ld]\n",
-				cpu,
+				"cpu=%d eff=%d [freq=%ld cap=%ld power_d0=%ld] -> [freq=%ld cap=%ld power_d0=%ld]\n",
+				cpu, efficiency,
 				sge_l0->cap_states[0].frequency,
 				sge_l0->cap_states[0].cap,
 				sge_l0->cap_states[0].power,
@@ -263,42 +277,67 @@ static int sched_energy_probe(struct platform_device *pdev)
 				sge_l0->cap_states[ncapstates - 1].power
 				);
 		}
+
+
+		dev_dbg(&pdev->dev,
+			"cpu=%d efficiency=%d max_frequency=%ld max_efficiency=%d cpu_max_capacity=%ld\n",
+			cpu, efficiency, max_frequencies[cpu], max_efficiency,
+			cpu_max_cap);
+
+		arch_update_cpu_capacity(cpu);
 	}
 
 	kfree(max_frequencies);
+
+	if (is_sge_valid) {
+		/*
+		 * Sched_domains might have built with default cpu capacity
+		 * values on bootup.
+		 *
+		 * Let's rebuild them again with actual cpu capacities.
+		 * And partition_sched_domain() expects update in cpu topology
+		 * to rebuild the domains, so make it satisfied..
+		 */
+		update_topology = 1;
+		rebuild_sched_domains();
+		update_topology = 0;
+
+		walt_sched_energy_populated_callback();
+	}
+
+	walt_map_freq_to_load();
+
 	dev_info(&pdev->dev, "Sched-energy-costs capacity updated\n");
 	return 0;
+
+exit_rcu_unlock:
+	rcu_read_unlock();
 
 exit:
 	if (ret != -EPROBE_DEFER)
 		dev_err(&pdev->dev, "error=%d\n", ret);
+
 	kfree(max_frequencies);
 	return ret;
 }
 
+static const struct of_device_id of_sched_energy_dt[] = {
+	{
+		.compatible = "sched-energy",
+	},
+	{ }
+};
+
 static struct platform_driver energy_driver = {
 	.driver = {
 		.name = "sched-energy",
+		.of_match_table = of_sched_energy_dt,
 	},
 	.probe = sched_energy_probe,
 };
 
-static struct platform_device energy_device = {
-	.name = "sched-energy",
-};
-
 static int __init sched_energy_init(void)
 {
-	int ret;
-
-	ret = platform_device_register(&energy_device);
-	if (ret)
-		pr_err("%s device_register failed:%d\n", __func__, ret);
-	ret = platform_driver_register(&energy_driver);
-	if (ret) {
-		pr_err("%s driver_register failed:%d\n", __func__, ret);
-		platform_device_unregister(&energy_device);
-	}
-	return ret;
+	return platform_driver_register(&energy_driver);
 }
 subsys_initcall(sched_energy_init);
