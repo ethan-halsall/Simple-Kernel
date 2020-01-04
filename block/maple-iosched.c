380d2b76211626f4d583e6d7c7cc0c8467a636e1
ethan-halsall <ethan.halsall@gmail.com>
2020-01-04 12:23:43 +0000

    block: maple-iosched: adapt msm_drm_notifier for xiaomi 845
    Xiaomi 845 does not support special blank and unblank states.
    
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -278,12 +278,11 @@ static int msm_drm_notifier_cb(struct notifier_block *nb,
 	blank = *(int *)(evdata->data);
 	mdata->display_on=true;
 
- 	if (((blank == MSM_DRM_BLANK_POWERDOWN)
+ 	if ((blank == MSM_DRM_BLANK_POWERDOWN)
 		&& (event == MSM_DRM_EARLY_EVENT_BLANK))
-		|| (blank == MSM_DRM_BLANK_NORMAL))
 		mdata->display_on = false;
 
-	if ((blank == MSM_DRM_BLANK_UNBLANK_CUST)
+	if ((blank == MSM_DRM_BLANK_UNBLANK)
 		&& (event == MSM_DRM_EARLY_EVENT_BLANK))
 		mdata->display_on = true;
 
