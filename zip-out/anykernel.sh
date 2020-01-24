e36df6d01cec7f4d9e864290c3fe4a0998c80430
Ethan Halsall <ethan.halsall@gmail.com>
2020-01-24 11:39:33 +0000

    zip-out: remove duplicated ramdisk changes
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- zip-out/anykernel.sh
+++ zip-out/anykernel.sh
@@ -30,12 +30,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 ## AnyKernel install
 dump_boot;
 
