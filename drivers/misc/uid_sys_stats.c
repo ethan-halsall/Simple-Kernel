3871be175479f38cab80e63fa0f5b65369fe8029
DennySPB <dennyspb@gmail.com>
2020-03-24 09:54:12 +0300

    drivers: misc: uid_sys_stats: convert cputime to nsec
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/misc/uid_sys_stats.c
+++ drivers/misc/uid_sys_stats.c
@@ -334,8 +334,8 @@ static int uid_cputime_show(struct seq_file *m, void *v)
 	struct uid_entry *uid_entry = NULL;
 	struct task_struct *task, *temp;
 	struct user_namespace *user_ns = current_user_ns();
-	cputime_t utime;
-	cputime_t stime;
+	u64 utime;
+	u64 stime;
 	unsigned long bkt;
 	uid_t uid;
 
@@ -365,15 +365,12 @@ static int uid_cputime_show(struct seq_file *m, void *v)
 	rcu_read_unlock();
 
 	hash_for_each(hash_table, bkt, uid_entry, hash) {
-		cputime_t total_utime = uid_entry->utime +
-							uid_entry->active_utime;
-		cputime_t total_stime = uid_entry->stime +
-							uid_entry->active_stime;
-		seq_printf(m, "%d: %llu %llu\n", uid_entry->uid,
-			(unsigned long long)jiffies_to_msecs(
-				cputime_to_jiffies(total_utime)) * USEC_PER_MSEC,
-			(unsigned long long)jiffies_to_msecs(
-				cputime_to_jiffies(total_stime)) * USEC_PER_MSEC);
+	u64 total_utime = uid_entry->utime +
+			    uid_entry->active_utime;
+	u64 total_stime = uid_entry->stime +
+			    uid_entry->active_stime;
+	seq_printf(m, "%d: %llu %llu\n", uid_entry->uid,
+			ktime_to_us(total_utime), ktime_to_us(total_stime));
 	}
 
 	rt_mutex_unlock(&uid_lock);
@@ -628,7 +625,7 @@ static int process_notifier(struct notifier_block *self,
 {
 	struct task_struct *task = v;
 	struct uid_entry *uid_entry;
-	cputime_t utime, stime;
+	u64 utime, stime;
 	uid_t uid;
 
 	if (!task)
