a01efe2f236518a2a58dd4460a1c7269621794ac
DennySPB <dennyspb@gmail.com>
2020-03-30 17:33:46 +0300

    Revert "softirq: defer softirq processing to ksoftirqd if CPU is busy with RT"
    This reverts commit f332a9d53e334f222f3e3b0f790ad1610d400bf8.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2658,7 +2658,6 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
-extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -2670,10 +2669,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
-}
-static inline bool cpupri_check_rt(void)
-{
-	return false;
 }
 #endif
 static inline int sched_update_freq_max_load(const cpumask_t *cpumask)
